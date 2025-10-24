4e491fa57895f23c8095fc9dbd772e985e87fe6c
Al Nikolov <roottoorfieuorg@gmail.com>
2025-10-24 15:20:45 +0300

    WIP: silli game

---
+++ silly_game.py
@@ -0,0 +1,7 @@
+# Player names a number
+player_number = int(input('Enter a number: '))
+# Computer adds 6
+computer_number = 6 + player_number
+# Computer tells its number and wins
+print('My number is:', computer_number, 'I WON!')
+# loop over the above
