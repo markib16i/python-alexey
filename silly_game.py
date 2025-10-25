34305804120b00fb62fa8b05538bcb050ece0167
Al Nikolov <roottoorfieuorg@gmail.com>
2025-10-25 11:51:41 +0300

    Indefinite while, if/break/continue

--- silly_game.py
+++ silly_game.py
@@ -1,7 +1,20 @@
-# Player names a number
-player_number = int(input('Enter a number: '))
-# Computer adds 6
-computer_number = 6 + player_number
-# Computer tells its number and wins
-print('My number is:', computer_number, 'I WON!')
-# loop over the above
+while True:
+    # Player names a number
+    player_number = int(input('Enter a number: '))
+    # Computer adds 6
+    computer_number = 6 + player_number
+    # Computer tells its number and wins
+    print('My number is:', computer_number, 'I WON!')
+    # loop over the above
+
+    # Ask the player if they want to continue
+    player_answer = input("Do you want to play again? (y/n): ")
+
+    # If yes, continue the loop
+    if player_answer == 'y':
+        print("Let's play again then")
+        continue
+    else:
+        # If no, break the loop
+        print('OK then bye')
+        break
