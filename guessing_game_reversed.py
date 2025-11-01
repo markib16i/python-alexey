feb1c3ec3e8a327319122a5bc029f26e7d33a483
Al Nikolov <roottoorfieuorg@gmail.com>
2025-11-01 12:55:52 +0200

    WIP: guessing game reversed

--- guessing_game_reversed.py
+++ guessing_game_reversed.py
@@ -1,1 +1,14 @@
 player_number = int(input("Produce a number between 1 and hundred, type it here: "))
+current_range_start = 1
+current_range_end = 100
+while True:
+    current_range_middle = (current_range_start + current_range_end) / 2
+    print("I think that your number is", current_range_middle)
+
+    if player_number > current_range_middle:
+        print("But looks like your number is greater. ")
+    elif player_number < current_range_middle:
+        print("But looks like your number is lesser.")
+    else:
+        print("I WON! ")
+        break
