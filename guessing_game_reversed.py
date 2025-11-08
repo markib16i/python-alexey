09d1f45baef7d5ca94430fbde44bbf6710f650c2
Al Nikolov <roottoorfieuorg@gmail.com>
2025-11-08 12:56:45 +0200

    Guessing game reversed. WIP: Pig Latin

--- guessing_game_reversed.py
+++ guessing_game_reversed.py
@@ -2,13 +2,15 @@ player_number = int(input("Produce a number between 1 and hundred, type it here:
 current_range_start = 1
 current_range_end = 100
 while True:
-    current_range_middle = (current_range_start + current_range_end) / 2
+    current_range_middle = (current_range_start + current_range_end) // 2
     print("I think that your number is", current_range_middle)
 
     if player_number > current_range_middle:
         print("But looks like your number is greater. ")
+        current_range_start = current_range_middle
     elif player_number < current_range_middle:
         print("But looks like your number is lesser.")
+        current_range_end = current_range_middle
     else:
         print("I WON! ")
         break
---
+++ pig_latin.py
@@ -0,0 +1,27 @@
+"""If a word starts with a consonant, move the first consonant (or group of consonants) to the end and add “ay”.
+
+    Example:
+
+        dog → ogday
+
+        school → oolschay
+
+If a word starts with a vowel, just add “way” or “yay” to the end.
+
+    Example:
+
+        apple → appleway
+
+        orange → orangeway
+"""
+
+# Get user phrase as an input
+english_phrase = input("Type a word/sentence/phrase. I am going to translate it to pig latin. ")
+
+# Separate phrase into words
+
+
+# Translate each word to Pig Latin
+
+
+# Assemble words back into a phrase, print the result
