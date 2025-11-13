6a03129f48c4a813eb2397c047972859e4b2bc8b
Al Nikolov <roottoorfieuorg@gmail.com>
2025-11-13 19:55:52 +0200

    WIP: pig latin -- vovels

--- pig_latin.py
+++ pig_latin.py
@@ -1,4 +1,5 @@
-"""If a word starts with a consonant, move the first consonant (or group of consonants) to the end and add “ay”.
+"""If a word starts with a consonant,
+move the first consonant (or group of consonants) to the end and add “ay”.
 
     Example:
 
@@ -19,9 +20,15 @@ If a word starts with a vowel, just add “way” or “yay” to the end.
 english_phrase = input("Type a word/sentence/phrase. I am going to translate it to pig latin. ")
 
 # Separate phrase into words
-
+words = english_phrase.split()
+# print(words)
 
 # Translate each word to Pig Latin
-
+# TODO: Make a new empty list for pig words
+for i in words:
+    # print(i)
+    if i[0] in 'aeiou':
+       pig_word = i + 'yay'
+    # TODO: append the new pig word to the list of pig words
 
 # Assemble words back into a phrase, print the result
