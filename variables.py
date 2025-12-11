3dec0a6773057e74d9780a20e7f637d7b50d083a
markib16i <markib16i@gmail.com>
2025-12-11 18:37:07 +0200

    Training

--- variables.py
+++ variables.py
@@ -1,16 +1,7 @@
-phrase = "I am in my room"
-print (phrase)
-words = phrase.split("m")
-print(words)
-broken_phrase = words[0] + words[1] + words[2]
-print(broken_phrase)
-
-extra_broken_phrase = input("Print a phrase")
-extra_broken_words = extra_broken_phrase.split("m")
-print(extra_broken_phrase)
-print(extra_broken_words)
-etr_broen_phrse = extra_broken_words[0] + extra_broken_words[1] + extra_broken_words[2]
-print(etr_broen_phrse)
-if "_" in words:
-    print(words)
-    print(etr_broen_phrse)
+task = "Elmira"
+number = 0
+for letter in task:
+    number = number + 1
+    if letter == "m":
+        print(number)
+    print(letter)
