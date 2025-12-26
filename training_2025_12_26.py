4f325fd05188c77388991f489a9ffd15f4fb8ded
Mark Bondarev <markib16i@gmail.com>
2025-12-26 11:34:01 +0200

    Training

---
+++ training_2025_12_26.py
@@ -0,0 +1,10 @@
+rows = int(input('how many rows?'))
+columns = int(input("how many columns?"))
+
+# for item in range(rows * columns):
+#     print("empty " + str(item + 1))
+
+# line = ""
+# for item in range(columns):
+#     line = line + "#"
+# print(line)
