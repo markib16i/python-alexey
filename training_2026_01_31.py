18e9c70d5039924cc1fbe51aec07b72372afcc94
Mark Bondarev <markib16i@gmail.com>
2026-02-04 19:01:31 +0200

    Training

---
+++ training_2026_01_31.py
@@ -0,0 +1,30 @@
+# question = 'say a number. '
+# number_first = int(input(question))
+# number_second = int(input(question))
+# total = number_second * number_first
+# print(total)
+
+# ask_cost = 'how much does it cost? '
+# cost = int(input(ask_cost))
+#
+# ask_count = 'how much milk do I need to buy? '
+# how_much = int(input(ask_count))
+#
+# print('I need ',cost * how_much)
+
+# how_much_candies = 'How much candies will eat '
+# count_mark = int(input(how_much_candies + "Mark?"))
+# count_oskar = int(input(how_much_candies + "Oskar?"))
+# count_ben = int(input(how_much_candies + "Ben?"))
+# total = count_mark + count_oskar + count_ben
+# print(total)
+
+# count_rows = int(input("how many rows are there? "))
+# count_columns = int(input("how many columns are there? "))
+# total = count_rows * count_columns
+# print("there is", total, "piles.")
+
+count_kids = int(input("how many kids where on he's birthday? "))
+count_money = int(input("how many money did they gave to him? "))
+total = count_kids * count_money
+print(total, "euro")
