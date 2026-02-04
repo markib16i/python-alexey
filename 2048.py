e7f864a3f9f27a70bfce906b130bacd153a9219e
Al Nikolov <roottoorfieuorg@gmail.com>
2026-02-04 19:51:23 +0200

    WIP: 2048

--- 2048.py
+++ 2048.py
@@ -2,6 +2,16 @@ import random
 from shutil import move
 
 
+def print_board(board):
+    for row in board:
+        for cell in row:
+            if cell is None:
+                visual_cell = ' '
+            else:
+                visual_cell = cell
+            print(f"| {visual_cell} ", end='')
+        print("|")
+
 def random_tile():
     if random.randint(0, 9) == 0:
         return 4
@@ -32,6 +42,7 @@ new_tile(board)
 new_tile(board)
 
 while True:  # Main game loop
+    print_board(board)
     our_question = "Enter the coordinates of the tile you want to push and the direction (w/a/s/d): "
     player_answer = input(our_question)
 
@@ -39,56 +50,54 @@ while True:  # Main game loop
     row_index = int(row_number) - 1
     column_index = int(column_number) - 1
 
-    # remember choice
-    new_row_index =     row_number
-    new_column_index = column_number
+    # # remember choice
+    # new_row_index = row_number
+    # new_column_index = column_number
 
     # Try to move further - according to user choice
     while True:
-        # save new before move
-        new_before_row_index = new_row_index
-        new_before_column_index = new_column_index
+        # set the next position to the current position
+        next_row_index = row_index
+        next_column_index = column_index
 
-        # update new by move
+        # calculate next position
         match direction:
             # up
             case "w":
-                new_row_index -= 1
+                next_row_index -= 1
             # bottom
             case "s":
-                new_row_index += 1
+                next_row_index += 1
             # left
             case 'a':
-                new_column_index -= 1
+                next_column_index -= 1
             # right
             case "d":
-                new_column_index += 1
+                next_column_index += 1
 
-        # 1. А есть ли тут такой же тайл
-        if board[new_row_index][new_column_index] == board[current_row_index][current_column_index]:
-            board[new_row_index][new_column_index] *= 2
+        # А НЕ ДОШЛИ ЛИ МЫ ДО КРАЯ
+        if not (
+            0 <= next_row_index <= 3 and
+            0 <= next_column_index <= 3
+        ):
             break
 
-        # 2. А есть ли тут другой тайл
+        tile = board[row_index][column_index]
+        destination_tile = board[next_row_index][next_column_index]
 
+        # А есть ли тут такой же тайл
+        if destination_tile == tile:
+            destination_tile *= 2
+            tile = None
+            break
 
-        # 3. А НЕ ДОШЛИ ЛИ МЫ ДО КРАЯ
-        if 0 < new_row_index < 4 and 0 < new_column_index < 3:
-            board[new_row_index][new_column_index] = board[current_row_index][current_column_index]
-            board[current_row_index][current_column_index] = None
+        # А есть ли тут другой тайл
+        if destination_tile != None:
             break
 
 
+        board[next_row_index][next_column_index] = tile
+        board[row_index][column_index] = None
+        row_index = next_row_index
+        column_index = next_column_index
 
-
-
-# Оформить этот кусочек, как функцию
-# Печатаем всю доску, как есть, ряд за рядом
-    for row in board:
-        for cell in row:
-            if cell is None:
-                visual_cell = ' '
-            else:
-                visual_cell = cell
-            print(f"| {visual_cell} ", end='')
-        print("|")
